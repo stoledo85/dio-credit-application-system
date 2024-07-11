64deb7cbab99691be688c6ccc4040bb8f062b37b
Sander Toledo <sanderaugusto@gmail.com>
2024-07-11 08:50:25 -0300

    atualização do modulo 3

--- src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
+++ src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
@@ -1,6 +1,7 @@
 package me.dio.credit.application.system.entity
 
 import jakarta.persistence.*
+import java.math.BigDecimal
 
 @Entity
 @Table(name="Customer")
@@ -10,6 +11,7 @@ data class Customer(
     @Column(nullable = false, unique = true) val cpf:String,
     @Column(nullable = false) var email:String="",
     @Column(nullable = false) var password:String="",
+    @Column(nullable = false) var income:BigDecimal=BigDecimal.ZERO,
     @Column(nullable = false) @Embedded var address: Address = Address(),
     @Column(nullable = false) @OneToMany(fetch = FetchType.LAZY, cascade = arrayOf(CascadeType.REMOVE,CascadeType.PERSIST), mappedBy = "customer" ) var credits: List<Credit> = mutableListOf(),
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id:Long? = null
